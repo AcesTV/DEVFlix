0ed317556b6ff07909e8254ea83d649e29983713
BREANT Nicolas <nicolas76.breant@hotmail.fr>
2021-03-25 10:31:52 +0100

    BASE MVC

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -1,7 +1,21 @@
 <?php
+namespace src\Controller;
 
+abstract class AbstractController {
+    protected $loader;
+    protected $twig;
 
-class AbstractController
-{
+    public function getTwig(){
+        return $this->twig;
+    }
+
+    public function __construct(){
+        $this->loader = new \Twig\Loader\FilesystemLoader($_SERVER["DOCUMENT_ROOT"]."/../templates");
+        $this->twig = new \Twig\Environment($this->loader,[
+            "debug" => true,
+            "cache" => $_SERVER["DOCUMENT_ROOT"]."/../var/cache"
+        ]);
+        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+    }
 
 }
--- src/Model/BDD.php
+++ src/Model/BDD.php
@@ -1,7 +1,35 @@
 <?php
+namespace src\Model;
+use PDO;
 
+class BDD {
+    private static $_instance = null;
 
-class BDD
-{
+    protected function __construct() { } // Constructeur en privé.
+    protected function __clone() { } // Méthode de clonage en privé aussi et vide pour empêcher le clonage
+
+    public static function initInstance()
+    {
+        try{
+            $hostname="localhost";
+            $username="root";
+            $password="";
+            $dbname="devflix";
+
+            SELF::$_instance = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $username, $password);
+            SELF::$_instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        }catch(\Exception $e){
+            SELF::$_instance = 'Erreur : '.$e->getMessage();
+        }
+    }
+
+    public static function getInstance()
+    {
+        if(SELF::$_instance == null){
+            SELF::initInstance();
+        }
+        return SELF::$_instance;
+    }
 
 }
